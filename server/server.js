0426f9ffe43196c8051adf81797710cdc2b7e608
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2022-10-22 13:38:16 +0200

    start of article displaying

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const feeds = require('../app');
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 6969;
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => res.render('main', { feeds }));
 app.get('/feeds', (req, res) => res.send(feeds));
 
 app.listen(PORT, () => console.log(`Server running on: http://localhost:${PORT}`));
