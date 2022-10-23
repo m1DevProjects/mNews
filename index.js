c5132c76b7d56114c8f5e3a6c94366f6df808a65
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2022-10-23 21:24:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const RssFeedEmitter = require('rss-feed-emitter');
-const feeder = new RssFeedEmitter(/* { skipFirstLoad: true } */);
+const feeder = new RssFeedEmitter({ skipFirstLoad: true });
 const feeds = require('./feeds');
 require('dotenv').config();
 const ms = require('ms');
@@ -34,10 +34,19 @@ events.forEach(e => {
             .setFooter({ text: 'mNews', iconURL: 'https://i.imgur.com/YkwM5EL.png' });
         if(enclosures && enclosures[0] && enclosures[0].url) embed.setThumbnail(enclosures[0].url);
         if(!enclosures[0].url || !enclosures) embed.setThumbnail('https://i.imgur.com/YkwM5EL.png')
+        let message = ' ';
+        if(e.type === 'swiat') message += `<@1033820877592211486>\n`
+        if(e.type === 'polska') message += `<@1033820908734922781>\n`
+        if(e.type === 'technologie') message += `<@1033820943354691687>\n`
+        if(e.type === 'sport') message += `<@1033820928259391518>\n`
+        if(e.provider === 'Polsat News') message += `<@1033820638504292353>\n`
+        if(e.provider === 'Interia') message += `<@1033820693390962779>\n`
+        if(e.provider === 'TVN24') message += `<@1033820717038448680>\n`
         e.webhookClient.send({
             username: 'mNews',
             avatarURL: 'https://i.imgur.com/YkwM5EL.png',
             embeds: [embed],
+            content: message
         });
     });
 })
